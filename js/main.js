1a332f35a934ac1148d519d955b5128383763f38
user41996 <gumangu12@gmail.com>
2023-03-08 12:35:51 +0700

    8 step

--- js/main.js
+++ js/main.js
@@ -1,15 +1,55 @@
- let app = new Vue({
-    el: '#app',
-    data: {
+ 
+ Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    }, 
+    template: `
+    <div class="product">
+    <div class="product-image">
+        <img :src="image" :alt="altText"/>
+    </div>
+
+    <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In stock</p>
+        <p v-else>Out of Stock</p>
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <p>Shipping: {{ shipping }}</p> 
+        <div
+                class="color-box"
+                v-for="(variant, index) in variants"
+                :key="variant.variantId"
+                :style="{ backgroundColor:variant.variantColor }"
+                @mouseover="updateProduct(index)"
+        ></div>
+        </div>
+
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+
+        <button
+                v-on:click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+        >
+            Add to cart
+        </button>
+    </div>
+
+</div>
+ `,
+ data() {
+    return {
         product: "Socks",
-        description:"A pair of warm, fuzzy socks",
         brand: 'Vue Mastery',
-        //image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
-        link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        // inStock: false,
-        // inventory: 100,
-        onSale:true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -24,35 +64,45 @@
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
                 variantQuantity: 0
             }
-         ],
-        selectedVariant: 0,
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
+        ],
+        cart: 0
+    }
+},
+methods: {
+    addToCart() {
+        this.cart += 1
     },
-    methods:{
-        addToCart() {
-            this.cart += 1
-        },
-        subtractToCart(){
-            this.cart -= 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-         },
+    updateProduct(index) {
+        this.selectedVariant = index;
+        console.log(index);
+    }
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product;
     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-         },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-         
-     },     
+    image() {
+        return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+    },
+    shipping() {
+        if (this.premium) {
+            return "Free";
+        } else {
+            return 2.99
+        }
+     }
+     
+}
+
  })
- 
- 
+
+ let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
+   
+ })
