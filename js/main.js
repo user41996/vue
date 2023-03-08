f30b0b9b0bcf03a77d844e99d5843676a8cf22af
user41996 <gumangu12@gmail.com>
2023-03-08 11:21:25 +0700

    4 step

--- js/main.js
+++ js/main.js
@@ -7,8 +7,21 @@
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 100,
+        // inventory: 100,
         onSale:true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        
     }
  })
  
