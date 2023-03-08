29ebe692cb572daf2648da331c86c622238e1a69
user41996 <gumangu12@gmail.com>
2023-03-08 11:53:00 +0700

    7 step

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
     data: {
         product: "Socks",
         description:"A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        //image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
+        // inStock: false,
         // inventory: 100,
         onSale:true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -15,14 +16,16 @@
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
          ],
-         
+        selectedVariant: 0,
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
     },
@@ -33,10 +36,23 @@
         subtractToCart(){
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
          },
-    }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+         },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+         
+     },     
  })
  
  
