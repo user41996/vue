6468c6dc7018ae66936f93f049485d18bfbba07c
user41996 <gumangu12@gmail.com>
2023-03-08 11:40:16 +0700

    6 step

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
+        inStock: false,
         // inventory: 100,
         onSale:true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
