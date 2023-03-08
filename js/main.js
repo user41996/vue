27257ac7ac063f55169d0d8634ede01132e2b919
user41996 <gumangu12@gmail.com>
2023-03-08 12:54:08 +0700

    10 step

--- js/main.js
+++ js/main.js
@@ -1,3 +1,71 @@
+Vue.component('product-review', {
+    template: `
+ 
+ <form class="review-form" @submit.prevent="onSubmit">
+ 
+ <p v-if="errors.length">
+  <b>Please correct the following error(s):</b>
+  <ul>
+    <li v-for="error in errors">{{ error }}</li>
+  </ul>
+ </p>
+ 
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+  </p>
+ 
+  <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+  </p>
+ 
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+ 
+  <p>
+    <input type="submit" value="Submit"> 
+  </p>
+ 
+ </form>
+  `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+        }
+    }
+ })
+ 
  
  Vue.component('product', {
     props: {
@@ -29,8 +97,16 @@
         ></div>
         </div>
 
-        <div class="cart">
-            <p>Cart({{ cart }})</p>
+        <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+            <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+            </li>
+            </ul>
         </div>
 
         <button
@@ -40,6 +116,7 @@
         >
             Add to cart
         </button>
+        
     </div>
 
 </div>
@@ -65,7 +142,8 @@
                 variantQuantity: 0
             }
         ],
-        cart: []
+        cart: [],
+        reviews:[],
     }
 },
 methods: {
@@ -75,7 +153,11 @@ methods: {
     updateProduct(index) {
         this.selectedVariant = index;
         console.log(index);
-    }
+    },
+    addReview(productReview) {
+        this.reviews.push(productReview)
+     }
+     
 },
 computed: {
     title() {
